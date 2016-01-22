7d63ef761f2f9db561039b8a5f8fe3863fba031e
Seyi Adekoya <sheyoootech@gmail.com>
2016-01-23 00:06:10 +0100

    fix api base

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ var app = angular.module('testify', ['ngMaterial',
 //app.constant('apiBase', "http://localhost/testify/api");
 app.constant('appUrl', "https://testify-staging.herokuapp.com");
 app.constant('appBase', "/");
-app.constant('apiBase', "https::testify-staging.herokuapp.com/api/v1");
+app.constant('apiBase', "https://testify-staging.herokuapp.com/api/v1");
 //app.constant('apiBase', "https://testify-for-testimonies.herokuapp.com/api");
 
 app.config(function(FacebookProvider, $httpProvider, RestangularProvider,
