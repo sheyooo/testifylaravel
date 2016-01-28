2f881f1195d78ddba456ffadd855963365d601dc
Seyi Adekoya <sheyoootech@gmail.com>
2016-01-28 16:42:49 +0100

    fix maxthon/safari display querk preferences page

--- public/js/controllers/appCtrl.js
+++ public/js/controllers/appCtrl.js
@@ -25,7 +25,8 @@ app.controller('AppCtrl', function($rootScope, $scope, $mdSidenav, $mdMedia,
     var l = cats.data.length;
     for(var i = 0; i < l; i++){
       cats.data[i].count = $filter('socialCounter')(cats.data[i].count);
-    };
+    }
+    
     $scope.categories = cats.data;
   });
 
